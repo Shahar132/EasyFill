46af09eeefbe6ff88c576eac6837d97725640ada
shani <shanihaker98@gmail.com>
2026-05-28 19:28:36 +0300

    added sections of the house assistance forms and feild and prograss bar

--- app/src/main/java/com/example/easyfill_project/forms_screens/HousingAssistanceFormScreen.kt
+++ app/src/main/java/com/example/easyfill_project/forms_screens/HousingAssistanceFormScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -79,17 +80,16 @@ fun HousingAssistanceFormScreen(navController: NavHostController) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-
             if (currentStep > 0) {
                 Button(onClick = { currentStep-- }) {
                     Text("חזור")
                 }
+            } else {
+                Spacer(modifier = Modifier.width(90.dp))
             }
 
             Button(onClick = {
-                if (currentStep < sections.size - 1) {
-                    currentStep++
-                }
+                if (currentStep < sections.size - 1) currentStep++
             }) {
                 Text("המשך")
             }
